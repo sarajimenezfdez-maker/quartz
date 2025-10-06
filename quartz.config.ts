8f9bdc37020d1ccd224dd886dd8ef0ee732377f6
sarajimenezfdez-maker <sara.jimenez.fdez@gmail.com>
2025-10-06 18:50:06 +0200

    funcion

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "sara jiménez",
+    pageTitle: "ruinas",
     pageTitleSuffix: "mi jardín",
     enableSPA: true,
     enablePopovers: true,
